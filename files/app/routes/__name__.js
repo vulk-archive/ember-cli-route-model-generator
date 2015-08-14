04a71cf1e708cba6c477e4cffea571ff3d006b41
wolfpack <wolfpack@vulk.co>
2015-08-14 03:05:44 -0400

    onFail is now aware of 500s and 404s

--- files/app/routes/__name__.js
+++ files/app/routes/__name__.js
@@ -5,9 +5,15 @@ export default Ember.Route.extend({
     var onSuccess = function(hash) {
       return hash;
     };
-    var onFail = function(xhr) {
-      var errors = Ember.$.parseJSON(xhr.responseText).errors;
-      return alert(errors);
+   var onFail = function(xhr) {
+      if (xhr.status === 500){
+        return alert('Internal server error. Contact administrator');
+      }  else if (xhr.status === 404) {
+        return true;
+      } else {
+        var errors = Ember.$.parseJSON(xhr.responseText).errors;
+        return alert(errors);
+      }
     };
     return this.store.findAll('<%= dasherizedModuleName %>').then(onSuccess, onFail);
   },
