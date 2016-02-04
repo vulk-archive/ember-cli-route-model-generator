9871d2a21c9b57fa147425c1aab5179565872590
wolfpack <wolfpack@vulk.co>
2016-02-04 00:40:32 -0500

    commented out code for client adapter that was screwing with reading API

--- README.md
+++ README.md
@@ -3,3 +3,10 @@ In the app/blueprints directory:
 ```
 git clone git@github.com:vulk/ember-cli-route-model-generator.git route-model
 ```
+
+To use 
+```
+ember g route-model <singular_model_name>
+```
+
+
--- files/app/adapters/__name__.js
+++ files/app/adapters/__name__.js
@@ -3,9 +3,12 @@ import Ember from 'ember';
 // export default DS.FixtureAdapter.extend({});
 
 export default DS.RESTAdapter.extend({
-  pathForType: function(type) {
-    var decamelized = Ember.String.decamelize(type);
-    return Ember.String.pluralize(decamelized);
-  },
+  // This is broken if there is no serializer for the rails API
+  // Keep this commented out until further notice
+  //
+  // pathForType: function(type) {
+  //   var decamelized = Ember.String.decamelize(type);
+  //   return Ember.String.pluralize(decamelized);
+  // },
 });
 
