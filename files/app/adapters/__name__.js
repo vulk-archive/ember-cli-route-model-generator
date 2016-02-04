fa710ac857bba375d35ca26360e4619e5dbc9532
wolfpack <wolfpack@vulk.co>
2016-02-04 15:43:20 -0500

    uncommented changes in the adapter.js

--- files/app/adapters/__name__.js
+++ files/app/adapters/__name__.js
@@ -3,12 +3,9 @@ import Ember from 'ember';
 // export default DS.FixtureAdapter.extend({});
 
 export default DS.RESTAdapter.extend({
-  // This is broken if there is no serializer for the rails API
-  // Keep this commented out until further notice
-  //
-  // pathForType: function(type) {
-  //   var decamelized = Ember.String.decamelize(type);
-  //   return Ember.String.pluralize(decamelized);
-  // },
+  pathForType: function(type) {
+    var decamelized = Ember.String.decamelize(type);
+    return Ember.String.pluralize(decamelized);
+  },
 });
 
